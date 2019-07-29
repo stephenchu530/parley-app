42605a208f47d1004f076a404957ed2d6d0506b2
Will Fritts <waf515@gmail.com>
2019-07-29 16:06:51 -0700

    changed it to Long instead of UUID

--- src/main/java/com/parley/parley/repository/StudentRepository.java
+++ src/main/java/com/parley/parley/repository/StudentRepository.java
@@ -3,8 +3,8 @@ package com.parley.parley.repository;
 import com.parley.parley.models.Student;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.UUID;
 
-public interface StudentRepository extends CrudRepository<Student, UUID> {
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
     Student findByUsername(String username);
 }
