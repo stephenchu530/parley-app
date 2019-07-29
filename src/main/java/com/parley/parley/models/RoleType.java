d3875450bece746a3b45c610f91ff56bf0081c86
Will Fritts <waf515@gmail.com>
2019-07-29 09:38:27 -0700

    base roletype model complete

--- src/main/java/com/parley/parley/models/RoleType.java
+++ src/main/java/com/parley/parley/models/RoleType.java
@@ -3,6 +3,8 @@ package com.parley.parley.models;
 
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -16,6 +18,42 @@ public class RoleType {
     @Column(unique = true)
     private String role;
 
+    @ManyToMany(mappedBy = "roleTypes", fetch = FetchType.EAGER)
+    private Set<Instructor> instructors = new HashSet<Instructor>();
+
+    //constructors
+    public RoleType(){}
+
+    public RoleType(String role){
+        this.role = role;
+    }
+
+    //getters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public Set<Instructor> getInstructors() {
+        return instructors;
+    }
 
 
+    //setters
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setInstructors(Set<Instructor> instructors) {
+        this.instructors = instructors;
+    }
 }
