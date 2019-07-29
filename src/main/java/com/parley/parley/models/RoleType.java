517f0a18f9a9510b800385ff4a4d9ce6bc4f3371
Will Fritts <waf515@gmail.com>
2019-07-29 07:53:34 -0700

    working on spring auth for admin rights

--- src/main/java/com/parley/parley/models/RoleType.java
+++ src/main/java/com/parley/parley/models/RoleType.java
@@ -1,7 +1,14 @@
 package com.parley.parley.models;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@DynamoDBTable(tableName = "roleType")
 public class RoleType {
+
+
+
 }
