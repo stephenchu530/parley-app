802c671d12208da867a19b7e40391968ce3590fa
Will Fritts <waf515@gmail.com>
2019-07-29 09:39:02 -0700

    added role repository

--- src/main/java/com/parley/parley/repository/RoleRepository.java
+++ src/main/java/com/parley/parley/repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.parley.parley.repository;
 
-public interface RoleRepository {
+import com.parley.parley.models.RoleType;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface RoleRepository extends CrudRepository<RoleType, UUID> {
+    RoleType findByRole(String role);
 }
