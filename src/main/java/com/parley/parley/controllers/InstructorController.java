86e0144d1ce54ffed69635ebea28a2801a50cee3
Will Fritts <waf515@gmail.com>
2019-07-30 12:10:39 -0700

    refactored

--- src/main/java/com/parley/parley/controllers/InstructorController.java
+++ src/main/java/com/parley/parley/controllers/InstructorController.java
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
 import java.util.ArrayList;
 
 
@@ -42,7 +44,7 @@ public class InstructorController {
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login(Principal principal){
         return "myprofile";
     }
 
